945b4e551acb73f7533c50d6172d0448107a5464
CarterMcCall53 <me@cmccall.dev>
2022-04-23 17:21:02 -0600

    Bug Fixes Fixed block selection

--- src/main/java/dev/cmccall/blockfloat/Events.java
+++ src/main/java/dev/cmccall/blockfloat/Events.java
@@ -13,7 +13,7 @@ public class Events implements Listener {
     @EventHandler
     public void onBlockClick(PlayerInteractEvent e) {
         if (e.getAction() == Action.LEFT_CLICK_AIR) {
-            var loc = e.getPlayer().getLocation().add(0, 1, 0);
+            var loc = e.getPlayer().getEyeLocation();
             var ray = e.getPlayer().getWorld().rayTraceBlocks(loc, loc.getDirection(), 200);
             if (ray == null) {
                 return;
@@ -22,11 +22,10 @@ public class Events implements Listener {
             if (block != null) {
                 var mat = block.getType();
                 block.setType(Material.AIR);
-                var fallblock = e.getPlayer().getWorld().spawnFallingBlock(block.getLocation().add(0, 1,0), mat.createBlockData());
+                var fallblock = e.getPlayer().getWorld().spawnFallingBlock(block.getLocation().add(0, 1, 0), mat.createBlockData());
                 fallblock.setGravity(false);
                 fallblock.setVelocity(new Vector(0, 0.2, 0));
             }
         }
-        // I love you guys!!!
     }
 }
